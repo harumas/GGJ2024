af1ab08b0e38372ec66fdb542d62374698626911
HarumaroJP <haruto09.game@gmail.com>
2024-01-27 04:09:31 +0900

    抽象イベントの実装

---
+++ GGJ2024/Assets/Scripts/Utility/RandomInt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Utility
+{
+    [Serializable]
+    public class RandomInt
+    {
+        [SerializeField] private int min;
+        [SerializeField] private int max;
+
+        public int MakeValue()
+        {
+            return Random.Range(min, max + 1);
+        }
+    }
+}
