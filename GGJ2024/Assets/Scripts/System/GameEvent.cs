a88df1ebfe43e09243b8a901adbac9caed9a555a
HarumaroJP <haruto09.game@gmail.com>
2024-01-27 01:05:04 +0900

    GameRouter追加

---
+++ GGJ2024/Assets/Scripts/System/GameEvent.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace System
+{
+    public class GameEvent
+    {
+        public void OnGameOver() { }
+
+        public void OnGameClear() { }
+    }
+}
