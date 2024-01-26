eb0e244d6dd2aafa93d795ef165eea1cd7ed4189
HarumaroJP <haruto09.game@gmail.com>
2024-01-27 07:29:41 +0900

    ノーマルイベントの実装

---
+++ GGJ2024/Assets/Scripts/Event/NormalEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Event
+{
+    public abstract class NormalEvent : AbstractEvent
+    {
+        [SerializeField] private string sentence;
+        [SerializeField] private string correct;
+
+        public string Sentence => sentence;
+        public string Correct => correct;
+    }
+}
