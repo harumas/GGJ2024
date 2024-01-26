a03653cd1bf7c553f72ee65b6b155ffc93180919
HarumaroJP <haruto09.game@gmail.com>
2024-01-26 09:52:36 +0900

    init project

---
+++ GGJ2024/Assets/Scripts/Utility/Locator.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+
+namespace Utility
+{
+    /// <summary>
+    /// インスタンスの保持しておくServiceLocator
+    /// </summary>
+    public static class Locator
+    {
+        private static readonly Dictionary<Type, object> serviceContainer;
+
+        static Locator()
+        {
+            serviceContainer = new Dictionary<Type, object>();
+        }
+
+        /// <summary>
+        /// インスタンスを取得する
+        /// </summary>
+        public static T Resolve<T>() where T : class
+        {
+            if (serviceContainer.TryGetValue(typeof(T), out object value))
+            {
+                return (T)value;
+            }
+
+            throw new NullReferenceException($"存在しないインスタンスを取得しています！\nType: {typeof(T)}");
+        }
+
+        /// <summary>
+        /// インスタンスを登録する
+        /// </summary>
+        public static void Register<T>(T instance) where T : class
+        {
+            serviceContainer[typeof(T)] = instance;
+        }
+
+        /// <summary>
+        /// インスタンスを登録解除する
+        /// </summary>
+        public static void UnRegister<T>() where T : class
+        {
+            if (!serviceContainer.ContainsKey(typeof(T)))
+            {
+                throw new NullReferenceException($"存在しないインスタンスを解除しています！\nType: {typeof(T)}");
+            }
+
+            serviceContainer.Remove(typeof(T));
+        }
+    }
+}
