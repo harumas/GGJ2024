f255508ca235d80f1da2401da04662b7eed67e32
HarumaroJP <haruto09.game@gmail.com>
2024-01-27 10:30:59 +0900

    フキダシ機能の追加

---
+++ GGJ2024/Assets/Scripts/Extension/ChatNormaly.cs
@@ -0,0 +1,22 @@
+﻿using Event;
+using UI;
+using UnityEngine;
+
+namespace Extension
+{
+    public class ChatNormaly : NormalEvent
+    {
+        [SerializeField] private ChatView chatView;
+
+        public override void Play()
+        {
+            chatView.SetText(Sentence.Replace("$", Correct));
+            chatView.gameObject.SetActive(true);
+        }
+
+        public override void Rewind()
+        {
+            chatView.gameObject.SetActive(false);
+        }
+    }
+}
